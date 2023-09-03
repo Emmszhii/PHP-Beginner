1e1bb0b6b9fb6a1292ede74bf0e7a257ab0fd361
Emmszhii <emmanuelmanalo707@gmail.com>
2023-09-03 16:49:41 +0800

    Environments and Configuration Flexibility

---
+++ Database.php
@@ -0,0 +1,28 @@
+<?php
+// Connect to db and execute query;
+class Database
+{
+    public $connection;
+
+    public function __construct($config, $username = 'root', $password = '')
+    {
+        $dsn = 'mysql:' . http_build_query($config, ',', ';'); //Refactored
+
+        // $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']};charset=utf8mb4";
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+    public function query($query)
+    {
+        $statement = $this->connection->prepare($query);
+        $statement->execute();
+        return $statement;
+    }
+}
+
+// dd($posts);
+
+// foreach ($posts as $post) {
+// echo $post['title'] . ' have an ID of ' . $post['id'] . '<br>';
+// }
--- index.php
+++ index.php
@@ -1,19 +1,11 @@
 <?php
 require 'functions.php';
 // require 'router.php';
+require 'Database.php';
+$config = require('config.php');
+$db = new Database($config['database']);
 
-// connect to msSql db.
-$dsn = "mysql:host=localhost;port=3306;user='root';dbname=myapp;charset=utf8mb4";
 
-$pdo = new PDO($dsn);
+$posts = $db->query("Select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$statement = $pdo->prepare("SELECT * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-// dd($posts);
-
-foreach ($posts as $post) {
-    echo $post['title'] . ' have an ID of ' . $post['id'] . '<br>';
-}
+dd($posts);
