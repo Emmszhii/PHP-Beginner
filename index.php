5ae2080b0465e69ca1fb4e83a7626623cecc98f3
Emmszhii <emmanuelmanalo707@gmail.com>
2023-09-03 15:29:39 +0800

    PDO connect to database

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
 require 'functions.php';
-require 'router.php';
+// require 'router.php';
+
+// connect to msSql db.
+$dsn = "mysql:host=localhost;port=3306;user='root';dbname=myapp;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+// dd($posts);
+
+foreach ($posts as $post) {
+    echo $post['title'] . ' have an ID of ' . $post['id'] . '<br>';
+}
